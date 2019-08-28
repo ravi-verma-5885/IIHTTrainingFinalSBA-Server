004d93ff254ac419a2b6a838f3638edf9d6f9cf5
ravi-verma-5885 <ravi.verma@cognizant.com>
2019-08-28 12:44:25 -0400

    Removed unreferenced files.

--- src/main/webapp/js/services.js
+++
@@ -1,7 +0,0 @@
-angular.module('projectApp.services', []).factory('User', function($resource) {
-  return $resource('http://localhost:8044/users/:id', { id: '@_id' }, {
-    update: {
-      method: 'PUT'
-    }
-  });
-});
